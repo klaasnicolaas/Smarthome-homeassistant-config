2926463404e6de4fffc4031f3d9836e3366a2865
Klaas Schoute <klaas_schoute@hotmail.com>
2018-10-03 00:57:24 +0200

    Update with the button

--- extras/bash/send-to-github.sh
+++ extras/bash/send-to-github.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-
 ####################################
 ## This script pushes my selected ##
 ## files to my github repo on a   ##
 ## new branch called 'master'     ##
 ####################################
-
 cd /home/homeassistant/.homeassistant/
 git add .
 git commit -a -m "Update with the button"
