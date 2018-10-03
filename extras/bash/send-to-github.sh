486056d5a7b63daf8c0cc32ce998ba65872ca35a
Klaas Schoute <klaas_schoute@hotmail.com>
2018-10-03 02:02:35 +0200

    Test 5

--- extras/bash/send-to-github.sh
+++ extras/bash/send-to-github.sh
@@ -3,11 +3,11 @@
 ####################################
 ## This script pushes my selected ##
 ## files to my github repo on a   ##
-## new branch called 'upload'     ##
+## new branch called 'master'     ##
 ####################################
 
 cd /home/homeassistant/.homeassistant
 git add .
-git commit -a -m "Update with the button"
+git commit -a -m "$1"
 git push origin master
 exit
