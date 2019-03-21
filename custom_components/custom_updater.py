ac8bd5300f8aeb380398adaf4e9944dae0221367
Klaas Schoute <klaas_schoute@hotmail.com>
2019-03-21 01:48:59 +0100

    :tada: Added custom updater component

--- custom_components/custom_updater.py
+++ custom_components/custom_updater.py
@@ -15,11 +15,11 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.components.http import HomeAssistantView
 from homeassistant.helpers.event import async_track_time_interval
 
-VERSION = '4.2.15'
+VERSION = '4.2.18'
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['pyupdate==1.3.5']
+REQUIREMENTS = ['pyupdate==1.3.6']
 
 CONF_TRACK = 'track'
 CONF_HIDE_SENSOR = 'hide_sensor'
@@ -159,7 +159,7 @@ class CustomCards():
         await self.cache_versions()
         await self.serve_dynamic_files()
 
-    async def force_reload(self):
+    async def force_reload(self, now=None):
         """Force data refresh"""
         _LOGGER.debug('CustomCards - force_reload')
         await self.pyupdate.force_reload()
@@ -312,9 +312,7 @@ class CustomCardsView(HomeAssistantView):
                 path=path)
             _LOGGER.debug(msg)
             resp = web.FileResponse(file)
-            resp.headers["Cache-Control"] = "no-store, no-cache, must-revalidate"
-            resp.headers["Pragma"] = "no-cache"
-            resp.headers["Expires"] = "0"
+            resp.headers["Cache-Control"] = "max-age=0, must-revalidate"
             return resp
         else:
             _LOGGER.error("Tried to serve up '%s' but it does not exist", file)
